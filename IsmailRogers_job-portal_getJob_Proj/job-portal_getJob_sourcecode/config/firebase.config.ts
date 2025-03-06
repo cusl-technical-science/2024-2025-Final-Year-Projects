e442be4ec77f01c884948ea09b7f8a75b8b254e3
iconicRog <144556165+iconicRog@users.noreply.github.com>
2025-03-06 15:07:44 +0000

    Update firebase.config.ts

--- IsmailRogers_job-portal_getJob_Proj/job-portal_getJob_sourcecode/config/firebase.config.ts
+++ IsmailRogers_job-portal_getJob_Proj/job-portal_getJob_sourcecode/config/firebase.config.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDMWn-Xh6RNdVWhsiFdWjv1lbCbinsbpqM",
-    authDomain: "job-portal-e9aa8.firebaseapp.com",
-    projectId: "job-portal-e9aa8",
-    storageBucket: "job-portal-e9aa8.firebasestorage.app",
-    messagingSenderId: "868064413485",
-    appId: "1:868064413485:web:50d3093a1c3985c05bb5a4"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
   };
    
    const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
